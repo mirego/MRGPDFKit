07667f623d5ad3bc0b5a42d250a7f2c105d75606
Luc Dion <Luc.DION@bdc.ca>
2016-12-06 14:18:19 -0500

    fix spacing

--- MRGPDFKit/Model/MRGPDFKitDocument.h
+++ MRGPDFKit/Model/MRGPDFKitDocument.h
@@ -34,7 +34,7 @@ typedef NS_ENUM(NSUInteger, MRGPDFKitDocumentRenderType) {
 - (UIImage *)imageFromPage:(NSUInteger)page width:(NSUInteger)width renderType:(MRGPDFKitDocumentRenderType)renderType;
 - (NSData *)flattenedDataForRenderType:(MRGPDFKitDocumentRenderType)renderType;
 - (NSData *)flattenedDataWithAnnotations:(NSArray *)annotations renderType:(MRGPDFKitDocumentRenderType)renderType;
-    - (NSData *)flattenedDataWithURL:(NSURL *)url annotations:(NSArray *)annotations renderType:(MRGPDFKitDocumentRenderType)renderType;
+- (NSData *)flattenedDataWithURL:(NSURL *)url annotations:(NSArray *)annotations renderType:(MRGPDFKitDocumentRenderType)renderType;
 - (NSUInteger)getPageCount;
 
 - (NSArray *)fieldsWithType:(MRGPDFKitFieldType)type;
